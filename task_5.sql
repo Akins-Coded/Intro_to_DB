4651c8505ed3eb6393945c62638edb6fa026d838
Akindipe Muheez <168924775+Akins-Coded@users.noreply.github.com>
2024-10-17 18:05:12 +0100

    Checker

---
+++ task_5.sql
@@ -0,0 +1,13 @@
+-- Use the database passed as an argument
+USE alx_book_store;
+INSERT INTO customer (
+    customer_id
+    customer_name,
+    email,
+    address
+    VALUE   
+    1
+    'Cole Baidoo'
+    'cbaidoo@sandtech.com'
+    '123 Happiness Ave'
+);
