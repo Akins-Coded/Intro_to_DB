abacf14d3cf59ee837f8d29157adb6ee4a7e3415
Akindipe Muheez <168924775+Akins-Coded@users.noreply.github.com>
2024-10-17 17:58:19 +0100

    Checker

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,15 @@
 -- Use the database passed as an argument
 USE alx_book_store;
 
--- Show all tables in the database
-SELECT * FROM Books;
+-- Select the table structure from information_schema
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
