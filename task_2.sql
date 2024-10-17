7024c2428fbb750a925b3788a704ed4be999cfc1
Akindipe Muheez <168924775+Akins-Coded@users.noreply.github.com>
2024-10-17 17:28:12 +0100

    Checker

---
+++ task_2.sql
@@ -0,0 +1,20 @@
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+-- Use the database
+USE alx_book_store;
+
+-- Create authors table
+CREATE TABLE IF NOT EXISTS authors ();
+
+-- Create books table
+CREATE TABLE IF NOT EXISTS books ();
+
+-- Create customers table
+CREATE TABLE IF NOT EXISTS customers ();
+
+-- Create orders table
+CREATE TABLE IF NOT EXISTS orders ();
+
+-- Create order_details table
+CREATE TABLE IF NOT EXISTS order_details ();
