efd02c5e0dc5ae83c3770c091788aa70f587dda6
Akindipe Muheez <168924775+Akins-Coded@users.noreply.github.com>
2024-10-17 16:33:58 +0100

    Checker

---
+++ alx_book_store.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE books (
+    book_id INT PRIMARY KEY NOT NULL,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE, 
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+     );
+
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY NOT NULL,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR (215),
+    email VARCHAR (215) UNIQUE,
+    address TEXT
+);
+
+CREATE TABLE orders(
+    order_id INT AUTO INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+    order_detail_id INT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES orders (order_id),
+    FOREIGN KEY (book_id) REFERENCES books (book_id)
+);
